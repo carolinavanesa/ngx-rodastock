0a47e02b9525aaea89765c76f723223e65d2570e
carolina <carolinavanesablanco88@gmail.com>
2022-02-18 13:53:57 -0300

    social link

--- src/app/@theme/components/footer/footer.component.ts
+++ src/app/@theme/components/footer/footer.component.ts
@@ -5,15 +5,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.scss'],
   template: `
     <span class="created-by">
-      Created with ♥ by CarolinaVanesaBlanco88... powered by <b><a href="https://akveo.page.link/8V2f" target="_blank">Akveo</a></b> 2021
+      Created with ♥ by CarolinaVanesaBlanco88... powered by
+      <b><a href="https://akveo.page.link/8V2f" target="_blank">Akveo</a></b>
+      2021
     </span>
-    <!-- <div class="socials">
-      <a href="#" target="_blank" class="ion ion-social-github"></a>
-      <a href="#" target="_blank" class="ion ion-social-facebook"></a>
-      <a href="#" target="_blank" class="ion ion-social-twitter"></a>
-      <a href="#" target="_blank" class="ion ion-social-linkedin"></a>
-    </div> -->
+
+    <div class="socials">
+      <a
+        href="
+  https://www.instagram.com/rodastock_ps/"
+        target="_blank"
+      >
+        <img
+          style="width: 22px"
+          src="https://www.edigitalagency.com.au/wp-content/uploads/new-instagram-logo-white-border-icon-png-large.png"
+        />
+      </a>
+
+      <nb-icon
+      class="ml-4"
+      icon="email-outline"
+      pack="eva"
+      (click)="mailTo()"
+    ></nb-icon>
+    </div>
   `,
 })
 export class FooterComponent {
+  mailTo() {
+    window.open('mailto:rodastockapp@gmail.com', '_blank')
+  }
 }
